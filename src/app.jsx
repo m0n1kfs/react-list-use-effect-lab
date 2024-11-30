dace74ad31f7d60627a2a28b477a66bdb92587b5
Monica Fernandez Salinas <monica.fernandez.salinas@gmail.com>
2024-11-30 10:59:47 +0100

    Iteración 1

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,16 @@
-import { Navbar } from "./components/ui"
+import { Routes, Route } from "react-router-dom";
+import Navbar from './components/ui/navbar/navbar.jsx';
+import Home from './components/ui/home/home.jsx';
 
 function App() {
-
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ui/home/home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+    return(
+        <div className="container mt-4">
+            <h1>Home</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Earum id velit consectetur veniam itaque, placeat minima nihil sint expedita ab eos saepe debitis praesentium, qui distinctio laboriosam fuga non iure!
+            </p>
+        </div>
+    );
+}
+
+export default Home;
